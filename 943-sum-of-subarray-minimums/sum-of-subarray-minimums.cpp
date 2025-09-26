43b28c57a40e6ab869ad792db7f8189f5d27b889
PRIYANSHU RAJ <149218578+priyanshu7455@users.noreply.github.com>
2025-09-26 15:54:31 +0530

    Time: 15 ms (69.95%) | Memory: 47.2 MB (62.46%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> nse1 =nextsmallerElement(arr);
+        vector<int> pse =PrevioussmallerElement(arr);
+        int mod=1000000007;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            int first=i-pse[i];
+            int second=nse1[i]-i;
+            long prod=(first*second)%mod;
+            prod=(prod*arr[i])%mod;
+            count=(count%mod+prod)%mod;
+
+        }
+        return count;
+    }
+
+     vector<int> nextsmallerElement(vector<int>& arr){
+        stack<int> st;
+        int n = arr.size();
+        vector<int> nse(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            nse[i]=st.empty() ? n : st.top();
+            st.push(i);//we are pusing index not number;
+        }
+        return nse;
+    }
+
+    vector<int> PrevioussmallerElement(vector<int>& arr){
+        stack<int> st;
+        int n = arr.size();
+        vector<int> pse(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            pse[i]=st.empty() ? -1 : st.top();
+            st.push(i);//we are pusing index not number;
+        }
+        return pse;
+    }
+
+    
+};
