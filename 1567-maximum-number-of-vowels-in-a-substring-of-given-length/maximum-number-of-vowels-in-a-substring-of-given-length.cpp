20beb8f7922383533a983ab574645a6fc90c879b
PRIYANSHU RAJ <149218578+priyanshu7455@users.noreply.github.com>
2026-01-26 00:11:55 +0530

    Time: 10 ms (30.20%) | Memory: 13 MB (88.89%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-
-    bool isvowel(char c){
-        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    bool isvowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
     int maxVowels(string s, int k) {
         int n=s.size();
-        int count=0;
-        
+        int count=0;//it will be count how may vouvel in ther baby;
+        //now make a windo first windo ;
         for(int i=0;i<k;i++){
             if(isvowel(s[i])){
                 count++;
             }
         }
         int maxi=count;
+
+        //now baby shift the widnow  and add right remove left;
         for(int i=k;i<n;i++){
             if(isvowel(s[i])){
                 count++;
             }
             if(isvowel(s[i-k])){
-                count--;
+                 count--;
             }
             maxi=max(maxi,count);
+
         }
         return maxi;
     }
